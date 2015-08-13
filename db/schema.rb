0a57100979d8f8d6d20ff430055348f1d962a10f
Marco Berardini <mberardini@evolution-man.com>
2015-08-13 16:22:47 -0700

    Added :name to User and Devise forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813172316) do
+ActiveRecord::Schema.define(version: 20150813230810) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150813172316) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
