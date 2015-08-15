be04d01708270d114dcc5be2f3166f1ac37da2d7
Marco Berardini <mberardini@evolution-man.com>
2015-08-14 18:05:25 -0700

    added review delete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,13 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_action :set_movie
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-
 
   def new
     @review = Review.new
+
   end
 
-  # GET /reviews/1/edit
-  def edit
-  end
-
-  # POST /reviews
-  # POST /reviews.json
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
@@ -30,8 +24,9 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reviews/1
-  # PATCH/PUT /reviews/1.json
+  def edit
+  end
+
   def update
     respond_to do |format|
       if @review.update(review_params)
@@ -44,27 +39,26 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # DELETE /reviews/1
-  # DELETE /reviews/1.json
+ 
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to @movie, notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_review
-      @review = Review.find(params[:id])
+      @review = @movie.reviews.find(params[:id])
     end
 
     def set_movie
       @movie = Movie.find(params[:movie_id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def review_params
       params.require(:review).permit(:rating, :comment)
     end
